354b5f9cd4da9c876f66655959486026f04f0ced
anrami <andrewramirez056@gmail.com>
2022-05-11 21:01:46 -0700

    form upload

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,15 @@
 const express = require('express')
 const router = express.Router()
+const Project = require('../models/project')
 
-router.get('/', (req, res) => {
-  res.render('index')
+router.get('/', async (req, res) => {
+  let projects
+  try {
+    projects = await Project.find().sort({ createdAt: 'desc' }).limit(10).exec()
+  } catch {
+    projects = []
+  }
+  res.render('index', { projects: projects })
 })
 
 module.exports = router
